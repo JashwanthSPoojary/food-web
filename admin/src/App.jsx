3bced6fc2fb068e7aaf9482db728f5bba5bc40f8
anonomous <125535128+Jashwanth1244@users.noreply.github.com>
2024-07-31 15:02:34 +0530

    url change to backend server

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 
 const App = () => {
-  const url = "http://localhost:4000"
+  const url = "https://food-backend-rtmk.onrender.com"
   return (
     <div>
       <ToastContainer/>
